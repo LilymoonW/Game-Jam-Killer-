3871827048ad8b1373c31bb2b4f078fb371d11d1
Emily Hsu <emilyhsu@Emilys-MacBook-Pro.local>
2024-03-30 15:24:45 -0400

    start screen

---
+++ World.gd
@@ -0,0 +1,19 @@
+extends Node2D
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
+
+
+func _on_door_1_pressed():
+	get_tree().change_scene_to_file("res://hotel rooms/room_101.tscn")
+
+
+func _on_door_2_pressed():
+	get_tree().change_scene_to_file("res://hotel rooms/room_102.tscn")
